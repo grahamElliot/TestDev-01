cf57dfb66d1925ef997c9e1ec76979b6a08f4c53
Graham Elliott <elliogh1@gmail.com>
2020-02-28 16:47:34 +0000

    4

--- WindowsApp1/WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -22,10 +22,46 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.fileName = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(21, 32)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(68, 20)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'fileName
+        '
+        Me.fileName.Location = New System.Drawing.Point(114, 32)
+        Me.fileName.Name = "fileName"
+        Me.fileName.Size = New System.Drawing.Size(198, 20)
+        Me.fileName.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.fileName)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents fileName As TextBox
 End Class
--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form1
+    Public traceTrap As UInt16
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OpenFileDialog1.Title = "Please Select a File to examine"
+        OpenFileDialog1.InitialDirectory = "C:"
+        OpenFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
 
+        OpenFileDialog1.ShowDialog()
+        Me.fileName.Text = OpenFileDialog1.FileName.ToString
+        Me.fileName.Update()
+        traceTrap = 1
+    End Sub
 End Class
