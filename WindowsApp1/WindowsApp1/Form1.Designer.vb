adeb69360613faec8a6ac56b1188c8448112305f
Graham Elliott <elliogh1@gmail.com>
2020-03-06 14:24:00 +0000

    5

--- WindowsApp1/WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -28,11 +28,16 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.textSepBox = New System.Windows.Forms.ListBox()
+        Me.fieldSepBox = New System.Windows.Forms.ListBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.checkFile = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(24, 75)
+        Me.Button1.Location = New System.Drawing.Point(31, 217)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(68, 20)
         Me.Button1.TabIndex = 0
@@ -45,9 +50,9 @@ Partial Class Form1
         '
         'fileName
         '
-        Me.fileName.Location = New System.Drawing.Point(98, 75)
+        Me.fileName.Location = New System.Drawing.Point(103, 217)
         Me.fileName.Name = "fileName"
-        Me.fileName.Size = New System.Drawing.Size(198, 20)
+        Me.fileName.Size = New System.Drawing.Size(212, 20)
         Me.fileName.TabIndex = 1
         '
         'Label1
@@ -77,11 +82,59 @@ Partial Class Form1
         Me.Label3.TabIndex = 7
         Me.Label3.Text = "The multi-value seperator must be ; (semi-colon) in this version."
         '
+        'textSepBox
+        '
+        Me.textSepBox.FormattingEnabled = True
+        Me.textSepBox.Location = New System.Drawing.Point(31, 107)
+        Me.textSepBox.Name = "textSepBox"
+        Me.textSepBox.Size = New System.Drawing.Size(125, 95)
+        Me.textSepBox.TabIndex = 8
+        '
+        'fieldSepBox
+        '
+        Me.fieldSepBox.FormattingEnabled = True
+        Me.fieldSepBox.Location = New System.Drawing.Point(197, 107)
+        Me.fieldSepBox.Name = "fieldSepBox"
+        Me.fieldSepBox.Size = New System.Drawing.Size(120, 95)
+        Me.fieldSepBox.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(28, 80)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(113, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Select Text Seperator."
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(194, 80)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(114, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Select Field Seperator."
+        '
+        'checkFile
+        '
+        Me.checkFile.Location = New System.Drawing.Point(33, 262)
+        Me.checkFile.Name = "checkFile"
+        Me.checkFile.Size = New System.Drawing.Size(80, 34)
+        Me.checkFile.TabIndex = 12
+        Me.checkFile.Text = "Check File"
+        Me.checkFile.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.checkFile)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.fieldSepBox)
+        Me.Controls.Add(Me.textSepBox)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
@@ -100,4 +153,9 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
+    Friend WithEvents textSepBox As ListBox
+    Friend WithEvents fieldSepBox As ListBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents checkFile As Button
 End Class
--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -2,11 +2,84 @@
 
 Public Class Form1
     Public traceTrap As UInt16
+    Public pubTextDelim As String
+    Public pubFieldDelim As String
+    Public pubMultiValDelim As String = ";"
+    Public pubParamComponents()
+    Public pubTSText(10) As String
+    Public pubFSText(10) As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim counter1 As UInt16 = 0
+
+        '/Set seperator character selection options for display
+        pubTSText(0) = """ (Double Quotes)"
+        pubTSText(1) = "char 2"
+        pubTSText(2) = "char 3"
+        pubTSText(3) = "char 4"
+        pubTSText(4) = "char 5"
+        pubTSText(5) = "char 6"
+        pubTSText(6) = "char 7"
+        pubTSText(7) = "char 8"
+        pubTSText(8) = "char 9"
+        pubTSText(9) = "char 10"
+
+        pubFSText(0) = ", (Comma)"
+        pubFSText(1) = "char 2"
+        pubFSText(2) = "char 3"
+        pubFSText(3) = "char 4"
+        pubFSText(4) = "char 5"
+        pubFSText(5) = "char 6"
+        pubFSText(6) = "char 7"
+        pubFSText(7) = "char 8"
+        pubFSText(8) = "char 9"
+        pubFSText(9) = "char 10"
+
+        For counter1 = 0 To 9
+            textSepBox.Items.Add(pubTSText(counter1))
+            fieldSepBox.Items.Add(pubFSText(counter1))
+        Next counter1
+    End Sub
+    Private Sub textSepBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles textSepBox.SelectedIndexChanged
+        MsgBox("Text Seperator is " + textSepBox.SelectedItem.ToString())
+    End Sub
+    Private Sub fieldSepBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles fieldSepBox.SelectedIndexChanged
+        MsgBox("Field Seperator is " + fieldSepBox.SelectedItem.ToString())
+    End Sub
+    Private Sub checkFile_Click(sender As Object, e As EventArgs) Handles checkFile.Click
+        Dim fileReader As StreamReader
+        fileReader = My.Computer.FileSystem.OpenTextFileReader(Me.fileName.Text)
+        Dim stringReader As String = ""
+
+        '/ Read first line which must contain headings,correctly formatted.
+        stringReader = fileReader.ReadLine()
+        '/ The first character must be a text seperator.
+        pubTextDelim = stringReader.Substring(0, 1)
+        '/ Validate text delimiter.
+        '/ cannot be ; which is multiValue delimiter
+        '/ won't be 0-9, a-z or common punctuation characters.
+        pubParamComponents = Split(stringReader, pubTextDelim, -1, vbTextCompare)
+        '/if the text delimiter is correct then:
+        '/ cell(0) should be null, cell(1),(3),(5) etc. should contain field names, cell(2),(4),(6) etc should contain the field seperator, last cell should be null.
+        If pubParamComponents(0) = "" Then
+            traceTrap = 1
+        End If
+
+        traceTrap = 1
+
+        Do Until fileReader.EndOfStream
+            stringReader = fileReader.ReadLine()
+        Loop
+
+        MsgBox("The last line of the file is " & stringReader)
+
+    End Sub
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         '/ Description
         '/ Delimited file format checker
         '/ Input = Delimited file in text format, first line must contain field headings, 
-        '/ formatted as {textDelim}Field-name-1{textDelim}{fieldDelim} .e.g. "Field-name-1","Field-name-2","Field-name-3" etc.
+        '/ formatted as {pubTextDelim}Field-name-1{pubTextDelim}{pubFieldDelim} .e.g. "Field-name-1","Field-name-2","Field-name-3" etc.
         '/ Field and text delimiters can be any character, multi-field delimiter is assumed to be ";" (semi colon) for this version.
 
         OpenFileDialog1.Title = "Please Select a File to examine"
@@ -17,16 +90,8 @@ Public Class Form1
         Me.fileName.Update()
         traceTrap = 1
 
-        Dim fileReader As StreamReader
-        fileReader = My.Computer.FileSystem.OpenTextFileReader(Me.fileName.Text)
-        Dim stringReader As String = ""
-        '/ Read first line which must contain headings.
-        stringReader = fileReader.ReadLine()
-        Do Until fileReader.EndOfStream
-            stringReader = fileReader.ReadLine()
-        Loop
-
-        MsgBox("The last line of the file is " & stringReader)
 
     End Sub
+
+
 End Class
