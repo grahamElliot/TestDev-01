d04fbadb0703b841bfcc9b82d5d894e954ebf926
Graham Elliott <elliogh1@gmail.com>
2020-03-27 15:52:59 +0000

    2703-01

--- WindowsApp1/WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -22,7 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.selectFile = New System.Windows.Forms.Button()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         Me.fileName = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
@@ -36,16 +36,26 @@ Partial Class Form1
         Me.Label2 = New System.Windows.Forms.Label()
         Me.fieldNamesList = New System.Windows.Forms.ListBox()
         Me.Label6 = New System.Windows.Forms.Label()
+        Me.headerLine = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.fileEncoding = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.checkData = New System.Windows.Forms.Button()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Button1
+        'selectFile
         '
-        Me.Button1.Location = New System.Drawing.Point(337, 107)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(68, 20)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Select file"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.selectFile.Location = New System.Drawing.Point(9, 50)
+        Me.selectFile.Name = "selectFile"
+        Me.selectFile.Size = New System.Drawing.Size(130, 20)
+        Me.selectFile.TabIndex = 0
+        Me.selectFile.Text = "Select data file."
+        Me.selectFile.UseVisualStyleBackColor = True
         '
         'OpenFileDialog1
         '
@@ -53,7 +63,7 @@ Partial Class Form1
         '
         'fileName
         '
-        Me.fileName.Location = New System.Drawing.Point(409, 107)
+        Me.fileName.Location = New System.Drawing.Point(144, 50)
         Me.fileName.Name = "fileName"
         Me.fileName.Size = New System.Drawing.Size(212, 20)
         Me.fileName.TabIndex = 1
@@ -61,7 +71,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(17, 19)
+        Me.Label1.Location = New System.Drawing.Point(6, 12)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(266, 13)
         Me.Label1.TabIndex = 5
@@ -70,67 +80,72 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(16, 37)
+        Me.Label3.Location = New System.Drawing.Point(6, 25)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(301, 13)
+        Me.Label3.Size = New System.Drawing.Size(204, 13)
         Me.Label3.TabIndex = 7
-        Me.Label3.Text = "The multi-value seperator must be ; (semi-colon) in this version."
+        Me.Label3.Text = "The multi-value seperator is ; (semi-colon)."
         '
         'textSepBox
         '
         Me.textSepBox.FormattingEnabled = True
-        Me.textSepBox.Location = New System.Drawing.Point(31, 107)
+        Me.textSepBox.Location = New System.Drawing.Point(14, 179)
         Me.textSepBox.Name = "textSepBox"
         Me.textSepBox.Size = New System.Drawing.Size(125, 56)
         Me.textSepBox.TabIndex = 8
+        Me.textSepBox.Visible = False
         '
         'fieldSepBox
         '
         Me.fieldSepBox.FormattingEnabled = True
-        Me.fieldSepBox.Location = New System.Drawing.Point(169, 107)
+        Me.fieldSepBox.Location = New System.Drawing.Point(161, 179)
         Me.fieldSepBox.Name = "fieldSepBox"
         Me.fieldSepBox.Size = New System.Drawing.Size(120, 56)
         Me.fieldSepBox.TabIndex = 9
+        Me.fieldSepBox.Visible = False
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(28, 80)
+        Me.Label4.Location = New System.Drawing.Point(11, 162)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(113, 13)
         Me.Label4.TabIndex = 10
         Me.Label4.Text = "Select Text Seperator."
+        Me.Label4.Visible = False
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(194, 80)
+        Me.Label5.Location = New System.Drawing.Point(158, 162)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(114, 13)
         Me.Label5.TabIndex = 11
         Me.Label5.Text = "Select Field Seperator."
+        Me.Label5.Visible = False
         '
         'checkFile
         '
-        Me.checkFile.Location = New System.Drawing.Point(337, 133)
+        Me.checkFile.Location = New System.Drawing.Point(298, 179)
         Me.checkFile.Name = "checkFile"
         Me.checkFile.Size = New System.Drawing.Size(100, 30)
         Me.checkFile.TabIndex = 12
-        Me.checkFile.Text = "Check Header"
+        Me.checkFile.Text = "Verify field names."
         Me.checkFile.UseVisualStyleBackColor = True
+        Me.checkFile.Visible = False
         '
         'runProgress
         '
-        Me.runProgress.Location = New System.Drawing.Point(169, 190)
+        Me.runProgress.Location = New System.Drawing.Point(7, 435)
         Me.runProgress.Multiline = True
         Me.runProgress.Name = "runProgress"
-        Me.runProgress.Size = New System.Drawing.Size(619, 121)
+        Me.runProgress.Size = New System.Drawing.Size(781, 24)
         Me.runProgress.TabIndex = 13
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(194, 174)
+        Me.Label2.Location = New System.Drawing.Point(5, 418)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(74, 13)
         Me.Label2.TabIndex = 14
@@ -139,46 +154,132 @@ Partial Class Form1
         'fieldNamesList
         '
         Me.fieldNamesList.FormattingEnabled = True
-        Me.fieldNamesList.Location = New System.Drawing.Point(31, 190)
+        Me.fieldNamesList.Location = New System.Drawing.Point(10, 260)
         Me.fieldNamesList.Name = "fieldNamesList"
         Me.fieldNamesList.Size = New System.Drawing.Size(124, 121)
         Me.fieldNamesList.TabIndex = 15
+        Me.fieldNamesList.Visible = False
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(29, 174)
+        Me.Label6.Location = New System.Drawing.Point(7, 243)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(114, 13)
         Me.Label6.TabIndex = 16
         Me.Label6.Text = "Field Names in Header"
+        Me.Label6.Visible = False
+        '
+        'headerLine
+        '
+        Me.headerLine.BackColor = System.Drawing.SystemColors.Window
+        Me.headerLine.Location = New System.Drawing.Point(9, 92)
+        Me.headerLine.Multiline = True
+        Me.headerLine.Name = "headerLine"
+        Me.headerLine.ReadOnly = True
+        Me.headerLine.Size = New System.Drawing.Size(755, 37)
+        Me.headerLine.TabIndex = 30
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(7, 76)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(127, 13)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "Header line from data file."
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(3, 2)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(785, 413)
+        Me.TabControl1.TabIndex = 19
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.checkData)
+        Me.TabPage1.Controls.Add(Me.Label8)
+        Me.TabPage1.Controls.Add(Me.fileEncoding)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.Label6)
+        Me.TabPage1.Controls.Add(Me.headerLine)
+        Me.TabPage1.Controls.Add(Me.fieldNamesList)
+        Me.TabPage1.Controls.Add(Me.Label7)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.textSepBox)
+        Me.TabPage1.Controls.Add(Me.fieldSepBox)
+        Me.TabPage1.Controls.Add(Me.Label4)
+        Me.TabPage1.Controls.Add(Me.Label5)
+        Me.TabPage1.Controls.Add(Me.selectFile)
+        Me.TabPage1.Controls.Add(Me.checkFile)
+        Me.TabPage1.Controls.Add(Me.fileName)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(777, 387)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Select data file."
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(777, 370)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'fileEncoding
+        '
+        Me.fileEncoding.Location = New System.Drawing.Point(139, 136)
+        Me.fileEncoding.Name = "fileEncoding"
+        Me.fileEncoding.Size = New System.Drawing.Size(212, 20)
+        Me.fileEncoding.TabIndex = 31
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(7, 139)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(132, 13)
+        Me.Label8.TabIndex = 32
+        Me.Label8.Text = "Encoding used in data file."
+        '
+        'checkData
+        '
+        Me.checkData.Location = New System.Drawing.Point(161, 351)
+        Me.checkData.Name = "checkData"
+        Me.checkData.Size = New System.Drawing.Size(133, 30)
+        Me.checkData.TabIndex = 33
+        Me.checkData.Text = "Check data consistency."
+        Me.checkData.UseVisualStyleBackColor = True
+        Me.checkData.Visible = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.fieldNamesList)
+        Me.ClientSize = New System.Drawing.Size(800, 464)
+        Me.Controls.Add(Me.TabControl1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.runProgress)
-        Me.Controls.Add(Me.checkFile)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.fieldSepBox)
-        Me.Controls.Add(Me.textSepBox)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.fileName)
-        Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "CheckTextFile"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents selectFile As Button
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
     Friend WithEvents fileName As TextBox
     Friend WithEvents Label1 As Label
@@ -192,4 +293,12 @@ Partial Class Form1
     Friend WithEvents Label2 As Label
     Friend WithEvents fieldNamesList As ListBox
     Friend WithEvents Label6 As Label
+    Friend WithEvents headerLine As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents Label8 As Label
+    Friend WithEvents fileEncoding As TextBox
+    Friend WithEvents checkData As Button
 End Class
--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -2,39 +2,48 @@
 
 Public Class Form1
     Public traceTrap As UInt16
-    Public pubTextDelim As String
+
+    Public pubAlphanumCheck As String = "0123456789abcdefghijklmnopqrstuvwxyz"
     Public pubFieldDelim As String
+    Public pubFieldNames() As String
+    Public pubFSChosen As Boolean = False
+    Public pubFSText(10) As String
+    Public pubLastFieldNumber As UInt16 = 0
     Public pubMultiValDelim As String = ";"
     Public pubParamComponents()
+    Public pubTextDelim As String
+    Public pubTSChosen As Boolean = False
     Public pubTSText(10) As String
-    Public pubFSText(10) As String
-    Public pubFieldNames() As String
+    Public pubUDIChosen As Boolean = False
+
+
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim counter1 As UInt16 = 0
 
         '/Set seperator character selection options for display
-        pubTSText(0) = """ (Double Quotes)"
-        pubTSText(1) = Chr(&HFE) & " (Thorn Ascii 254)"
-        pubTSText(2) = "char 3"
-        pubTSText(3) = "char 4"
-        pubTSText(4) = "char 5"
-        pubTSText(5) = "char 6"
-        pubTSText(6) = "char 7"
-        pubTSText(7) = "char 8"
-        pubTSText(8) = "char 9"
-        pubTSText(9) = "char 10"
+        pubTSText(0) = "   (no text seperator)"
+        pubTSText(1) = """ (Double Quotes)"
+        pubTSText(2) = Chr(&H7E) & " (Tilde)"
+        pubTSText(3) = Chr(&HA6) & " (Broken Bar)"
+        pubTSText(4) = Chr(&HAC) & " (NOT sign)"
+        pubTSText(5) = Chr(&HB6) & " (Pilcrow sign)"
+        pubTSText(6) = Chr(&HFE) & " (Thorn Ascii 254)"
+        pubTSText(7) = ""
+        pubTSText(8) = ""
+        pubTSText(9) = ""
 
+        '/ Field seperators
         pubFSText(0) = ", (Comma)"
         pubFSText(1) = Chr(&H14) & " (DC4 Ascii 20)"
-        pubFSText(2) = "char 3"
-        pubFSText(3) = "char 4"
-        pubFSText(4) = "char 5"
-        pubFSText(5) = "char 6"
-        pubFSText(6) = "char 7"
-        pubFSText(7) = "char 8"
-        pubFSText(8) = "char 9"
-        pubFSText(9) = "char 10"
+        pubFSText(2) = Chr(&H7E) & " (Tilde)"
+        pubFSText(3) = Chr(&HA6) & " (Broken Bar)"
+        pubFSText(4) = Chr(&HAC) & " (NOT sign)"
+        pubFSText(5) = Chr(&HB6) & " (Pilcrow sign)"
+        pubFSText(6) = ""
+        pubFSText(7) = ""
+        pubFSText(8) = ""
+        pubFSText(9) = ""
 
         For counter1 = 0 To 9
             textSepBox.Items.Add(pubTSText(counter1))
@@ -44,26 +53,54 @@ Public Class Form1
     Private Sub textSepBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles textSepBox.SelectedIndexChanged
         Me.runProgress.Text = "Text Seperator is " + textSepBox.SelectedItem.ToString()
         pubTextDelim = textSepBox.SelectedItem.ToString.Substring(0, 1)
+        pubTSChosen = True
+        If pubFSChosen = True Then
+            Call check_seperators_differ()
+            Me.checkFile.Visible = True
+        End If
     End Sub
     Private Sub fieldSepBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles fieldSepBox.SelectedIndexChanged
         Me.runProgress.Text = Me.runProgress.Text & vbCrLf & "Field Seperator is " + fieldSepBox.SelectedItem.ToString()
         pubFieldDelim = fieldSepBox.SelectedItem.ToString.Substring(0, 1)
+        pubFSChosen = True
+        If pubTSChosen = True Then
+            Call check_seperators_differ()
+            Me.checkFile.Visible = True
+        End If
     End Sub
     Private Sub fieldNamesList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles fieldNamesList.SelectedIndexChanged
         Me.runProgress.Text = "Unique Document Identifier is " + fieldNamesList.SelectedItem.ToString()
+        pubUDIChosen = True
+        Me.checkData.Visible = True
     End Sub
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles selectFile.Click
         '/ Description
         '/ Delimited file format checker
         '/ Input = Delimited file in text format, first line must contain field headings, 
         '/ formatted as {pubTextDelim}Field-name-1{pubTextDelim}{pubFieldDelim} .e.g. "Field-name-1","Field-name-2","Field-name-3" etc.
         '/ Field and text delimiters can be any character, multi-field delimiter is assumed to be ";" (semi colon) for this version.
+        Dim dataString As String = ""
+        Dim fileReader As StreamReader
+        Dim stringReader As String = ""
+
         OpenFileDialog1.Title = "Please Select a File to examine"
         OpenFileDialog1.InitialDirectory = "C:"
         OpenFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
         OpenFileDialog1.ShowDialog()
         Me.fileName.Text = OpenFileDialog1.FileName.ToString
         Me.fileName.Update()
+        fileReader = My.Computer.FileSystem.OpenTextFileReader(Me.fileName.Text)
+        Me.fileEncoding.Text = fileReader.CurrentEncoding.ToString
+        Me.runProgress.Text = Me.runProgress.Text & vbCrLf & "Opening file " + Me.fileName.Text + " encoding is " + dataString
+        stringReader = fileReader.ReadLine()
+        fileReader.Close()
+        Me.headerLine.Text = stringReader
+        Me.runProgress.Update()
+        Me.Label4.Visible = True
+        Me.Label5.Visible = True
+        Me.textSepBox.Visible = True
+        Me.fieldSepBox.Visible = True
+        MsgBox("Please select a text seperator and a field seperator.")
     End Sub
     Private Sub checkFile_Click(sender As Object, e As EventArgs) Handles checkFile.Click
 
@@ -71,47 +108,99 @@ Public Class Form1
         Dim counter1 As UInt16 = 0
         Dim dataString As String = ""
         Dim fileReader As StreamReader
-
-        fileReader = My.Computer.FileSystem.OpenTextFileReader(Me.fileName.Text)
+        Dim Style, Title, Response
         Dim stringReader As String = ""
+        Dim firstChar As String = ""
+        Dim lastChar As String = ""
+
+        Me.Label6.Visible = True
+        Me.fieldNamesList.Visible = True
+        fileReader = My.Computer.FileSystem.OpenTextFileReader(Me.fileName.Text)
+
         dataString = fileReader.CurrentEncoding.ToString
         Me.runProgress.Text = Me.runProgress.Text & vbCrLf & "Opening file " + Me.fileName.Text + " encoding is " + dataString
         stringReader = fileReader.ReadLine()
-        Me.runProgress.Text = Me.runProgress.Text & vbCrLf & "Headings line is:" + vbCrLf + stringReader
+        Me.headerLine.Text = stringReader
         Me.runProgress.Update()
-
+        fileReader.Close()
         '/Try the selected field delimiter
         pubParamComponents = Split(stringReader, pubFieldDelim, -1, vbTextCompare)
 
         '/store the field names
         ReDim pubFieldNames(UBound(pubParamComponents))
+        pubLastFieldNumber = UBound(pubParamComponents)
         For counter1 = 0 To UBound(pubParamComponents)
-            pubFieldNames(counter1) = Replace(pubParamComponents(counter1), """", "", 1, -1, vbTextCompare)
+            If pubTextDelim <> " " Then
+                pubFieldNames(counter1) = Replace(pubParamComponents(counter1), pubTextDelim, "", 1, -1, vbTextCompare)
+            Else
+                pubFieldNames(counter1) = pubParamComponents(counter1)
+            End If
+
+            '/If no text delimiter has been specified, run some basic checks that none exist.
+            If pubTextDelim = " " Then
+                pubTSChosen = False
+                '/Check if the first and last characters are identical, if so check if they are a-z or 0-9
+                firstChar = LCase(pubFieldNames(counter1).Substring(0, 1))
+                lastChar = LCase(pubFieldNames(counter1).Substring(pubFieldNames(counter1).Length - 1, 1))
+                If firstChar = lastChar Then
+                    If Not pubAlphanumCheck.Contains(firstChar) Then
+                        Style = vbYesNo + MsgBoxStyle.MsgBoxHelp + vbDefaultButton2 ' Define buttons.
+                        Title = "Header Validation" ' Define title.
+                        Response = MsgBox("The first (" & firstChar & ") and last (" & lastChar & ") characters in this field name are identical and potentially text seperators" & vbCrLf & "Do you want to restart the run ad select a text seperator?", Style, Title)
+                        If Response = vbYes Then
+                            MsgBox("Please re-run with text seperator selected.")
+                            Application.Exit()
+                        End If
+                    End If
+                End If
+            End If
+
             assembledFieldNames = assembledFieldNames & "," & pubFieldNames(counter1)
             fieldNamesList.Items.Add(pubFieldNames(counter1))
         Next counter1
         Me.runProgress.Text = Me.runProgress.Text & vbCrLf & "Extracted field names are:"
         Me.runProgress.Text = Me.runProgress.Text & vbCrLf & assembledFieldNames
 
-
-        Dim Style, Title, Response, MyString
         Style = vbYesNo + MsgBoxStyle.MsgBoxHelp + vbDefaultButton2 ' Define buttons.
         Title = "Header Validation" ' Define title.
         Response = MsgBox("Do the fields match the header line?", Style, Title)
 
         If Response = vbNo Then
-            MyString = "No" ' Perform some action.
+            MsgBox("Please re-run using different field seperator.")
+            Application.Exit()
         Else
-
+            MsgBox("Please select the unique document identifier field" & vbCrLf & " from the Field Names list.")
             traceTrap = 1
-
-            Do Until fileReader.EndOfStream
-                stringReader = fileReader.ReadLine()
-            Loop
-
-            MsgBox("The last line of the file is " & stringReader)
         End If
     End Sub
 
+    Private Sub checkData_Click(sender As Object, e As EventArgs) Handles checkData.Click
+        Dim assembledFieldNames As String = ""
+        Dim counter1 As UInt16 = 0
+        Dim dataString As String = ""
+        Dim fileReader As StreamReader
+        Dim stringReader As String = ""
 
+        fileReader = My.Computer.FileSystem.OpenTextFileReader(Me.fileName.Text)
+
+        dataString = fileReader.CurrentEncoding.ToString
+        Me.runProgress.Text = Me.runProgress.Text & vbCrLf & "Opening file " + Me.fileName.Text + " encoding is " + dataString
+        '/ Skip over headings line.
+        stringReader = fileReader.ReadLine()
+
+        Do Until fileReader.EndOfStream
+            stringReader = fileReader.ReadLine()
+            pubParamComponents = Split(stringReader, pubFieldDelim, -1, vbTextCompare)
+
+        Loop
+
+        MsgBox("The last line of the file is " & stringReader)
+    End Sub
+    Private Sub check_seperators_differ()
+        '/The text and field seperators can't be the same character.
+        If pubTextDelim = pubFieldDelim Then
+            MsgBox("The text and field seperators can't be the same character, please fix and re-run.")
+            Application.Exit()
+        End If
+    End Sub
 End Class
